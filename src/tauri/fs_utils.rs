cd4888dd871aec7c11ef333e180507dc0cee440c
Peter N. Robinson <peter.robinson@bih-charite.de>
2026-06-24 17:55:52 +0200

    Loading ontology

--- src/tauri/fs_utils.rs
+++ src/tauri/fs_utils.rs
@@ -1,4 +1,6 @@
 use tauri_plugin_fs::FilePath;
+use ontolius::{io::OntologyLoaderBuilder, ontology::csr::FullCsrOntology};
+use std::sync::Arc;
 
 /// Converts a generic Tauri file-picker `FilePath` into a standard `String`.
 pub fn get_full_path_as_str(file_path: FilePath) -> Result<String, String> {
@@ -7,4 +9,11 @@ pub fn get_full_path_as_str(file_path: FilePath) -> Result<String, String> {
         .ok_or_else(|| "Failed to extract system path from FilePath entry".to_string())?;
 
     Ok(path.to_string_lossy().to_string())
+}
+
+
+pub fn load_ontology(json_path: &str) -> Result<Arc<FullCsrOntology>, Box<dyn std::error::Error>> {
+    let loader = OntologyLoaderBuilder::new().obographs_parser().build();
+    let onto: FullCsrOntology = loader.load_from_path(json_path)?;
+    Ok(Arc::new(onto))
 }
--- src/tauri/mod.rs
+++ src/tauri/mod.rs
@@ -5,4 +5,5 @@ pub mod models;
 
 
 pub use fs_utils::get_full_path_as_str;
+pub use fs_utils::load_ontology;
 pub use models::OntologyLoadEvent;
